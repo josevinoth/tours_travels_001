1de0ad558e4762ff1836ef2a610f6ea302c43e6f
BLUE CYBER(003) <hariharasudhanh968@gmail.com>
2024-08-08 00:08:23 -0700

    first commit hari (06-Aug-2024)

---
+++ tt_project/tt_app/sub_views/marks_add_view.py
@@ -0,0 +1,28 @@
+from ..forms import marksaddForm
+from ..models import marks_info
+from django.shortcuts import render, redirect
+
+#@login_required(login_url='login_page')
+def marks_add(request,registration_id=0):
+    if request.method == "GET":
+        if registration_id == 0:
+            form = marksaddForm()
+        else:
+            reg=marks_info.objects.get(pk=registration_id)
+            form = marksaddForm(instance=reg)
+
+        context={
+                'form':form,
+                }
+        return render(request, "tt_html/marks.html",context)
+    else:
+        if registration_id == 0:
+            form = marksaddForm(request.POST)
+        else:
+            reg = marks_info.objects.get(pk=registration_id)
+            form = marksaddForm(request.POST,instance=reg)
+
+        if form.is_valid():
+            form.save()
+        # return redirect('/SMS/bay_list')
+        return redirect(request.META['HTTP_REFERER'])
